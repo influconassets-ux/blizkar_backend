ecb47c78ea92c8a2b3782d212c8841269f1ddf4e
Souvik Saha <souviksaha.dmj@gmail.com>
2026-05-09 10:45:42 +0530

    Delete scratch_msg.js

--- scratch_msg.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
-
-mongoose.connect(process.env.MONGO_URI).then(async () => {
-    const db = mongoose.connection.db;
-    const msgs = await db.collection('messages').find({ messageType: { $in: ['image', 'video'] } }).sort({createdAt: -1}).limit(5).toArray();
-    console.log(JSON.stringify(msgs, null, 2));
-    process.exit(0);
-});
